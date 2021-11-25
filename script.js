46f1bd5572e766ff82cb0d1316b79e6c93a6df3f
Rishav Mitra <rishavmitra10@gmail.com>
2021-11-25 16:42:43 +0000

    storing notes in localStorage

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 const buttonElement = document.getElementById("submit");
 buttonElement.addEventListener("click", addNote);
 
+loadNotes();
+
 function addNote() {
   const noteText = document.getElementById("noteText").value;
   if(noteText.trim() === "") {
     return;
   }
+
+  if(localStorage.getItem('listNotes') == null) {
+    localStorage.setItem('listNotes', '[]');
+  }
+
+  const oldNotes = JSON.parse(localStorage.getItem('listNotes'));
+  oldNotes.push(noteText);
+  localStorage.setItem('listNotes', JSON.stringify(oldNotes));
+  
+  addNoteToPage(noteText);
+}
+
+function loadNotes() {
+  if(localStorage.getItem('listNotes') != null) {
+    const notes = JSON.parse(localStorage.getItem('listNotes'));
+    for(i=0; i<notes.length; i++) {
+      addNoteToPage(notes[i]);
+    }
+  }
+}
+
+function addNoteToPage(text) {
   const newNote = document.createElement("div");
-  newNote.innerHTML = noteText;
+  newNote.innerHTML = text;
   newNote.classList.add("note");
   document.getElementsByClassName("notes")[0].appendChild(newNote);
 }
